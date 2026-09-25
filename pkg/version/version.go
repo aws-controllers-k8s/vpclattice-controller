b48dfe10a9099f9554f8fed741a2fc3ff9e4c2ca
michaelhtm <98621731+michaelhtm@users.noreply.github.com>
2026-09-25 10:52:07 -0700

    chore: regenerate controller and add recommended iam policy

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -27,6 +27,6 @@ var (
 // that generated this controller's code. They are baked in at code-generation
 // time and are immutable for the life of the generated code.
 const (
-	ACKGenerateVersion   = "v0.64.0"
-	ACKGenerateBuildDate = "2026-09-22T17:14:17Z"
+	ACKGenerateVersion   = "v0.64.0-2-g6bd51fa"
+	ACKGenerateBuildDate = "2026-09-25T17:24:24Z"
 )
